4799e027e9f7589cf450527e98a2a151acd4f185
Tim Schumacher <tim@datenknoten.me>
2020-02-08 09:35:30 +0100

    chore: add a script to generate some test data

---
+++ scripts/democontent.ts
@@ -0,0 +1,71 @@
+/*!
+ * @copyright Card Librarian Team 2020
+ */
+
+import { Logger } from '@flyacts/backend';
+import { UserEntity } from '@flyacts/backend-user-management';
+import config = require('config');
+import { serializeError } from 'serialize-error';
+import { createConnection } from 'typeorm';
+
+import { CardEntity } from '../src/entities/card.entity';
+import { SetEntity } from '../src/entities/set.entity';
+import { UserExtensionEntity } from '../src/entities/user-extension.entity';
+
+// tslint:disable-next-line:no-floating-promises
+(async function() {
+    const logger = new Logger();
+
+    logger.info('Adding some Democontent to work with');
+
+    try {
+        const typeOrmConfig = {
+            ...require('../ormconfig.json'),
+            ...config.get<Object>('database'),
+        };
+
+        const connection = await createConnection(typeOrmConfig);
+
+        const username = 'tom';
+        const firstname = 'Tom';
+        const lastname = 'Tester';
+        // tslint:disable-next-line
+        const password = '123456';
+        const email = 'tom.tester@test.test';
+
+        const userExtension = new UserExtensionEntity();
+        const user = new UserEntity();
+        userExtension.firstname = firstname;
+        userExtension.lastname = lastname;
+        user.username = username;
+        await user.setPassword(password);
+        user.email = email;
+        user.realm = 'app';
+
+        await connection.manager.save(user);
+
+        userExtension.user = user;
+
+        await connection.manager.save(userExtension);
+
+        const set = new SetEntity();
+        set.name = 'Unglued';
+        set.code = 'UGL';
+        set.importData = '{}';
+
+        await connection.manager.save(set);
+
+        const card = new CardEntity();
+        card.name = 'Chaos Confetti';
+        card.manaCost = '{4}';
+        card.types = 'Artifact';
+        card.importData = '{}';
+        card.set = set;
+        card.uuid = '6bd280d0-3259-4343-9203-3bda16444e7e';
+
+        await connection.manager.save(card);
+    } catch (error) {
+        logger.error('Failed to initialize democontent', serializeError(error));
+        process.exit(-1);
+    }
+})();
