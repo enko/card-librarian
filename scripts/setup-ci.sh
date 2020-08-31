99399ff2e72ffa0c6f167b543b955aa3d10fe663
Tim Schumacher <tim@datenknoten.me>
2020-08-31 20:19:01 +0200

    ci: restructure build

--- scripts/setup-ci.sh
+++ scripts/setup-ci.sh
@@ -8,10 +8,16 @@ export NVM_DIR="$HOME/.nvm"
 
 set +x
 
+export DOCKER_BUILDKIT=1
+
 . /usr/share/nvm/init-nvm.sh
 cd card-librarian
 nvm install
 nvm use --silent
+
+if [ ! -d node_modules ]; then
+    npm ci
+fi;
 
 function finish {
   curl -X POST $(< ~/slack-webhook-url) -d "{ \"text\": \"❌💣❌ Pipeline $JOB_ID at $JOB_URL failed\", \"format\": \"plain\", \"displayName\": \"Card Librarian CI\", \"avatarUrl\": \"https://mtg.tim.schumacher.im/assets/apple-touch-icon.png\" }" -H 'Content-Type: application/json'
